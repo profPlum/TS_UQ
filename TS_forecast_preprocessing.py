a7b88523d1187c70917d7c0782ad9556f043e832
Dwyer Deighan <dwyerdei@buffalo.edu>
2024-02-29 22:18:30 +0000

    TSAI_TS_Sampling_UP.ipynb (sampling based forward-UQ for TS) + various updates to core files

--- TS_forecast_preprocessing.py
+++ TS_forecast_preprocessing.py
@@ -14,11 +14,16 @@ find_dtype_cols = lambda df, dtypes: list(df.select_dtypes(include=dtypes).colum
 
 def find_TS_value_cols(df): # instead of df.columns[2:]
     TS_value_cols = find_dtype_cols(df, 'number')
+
+    # hacks to avoid "time_key" column
+    TS_value_cols = [col for col in TS_value_cols if not 'time' in col]
+    assert len(TS_value_cols)==1 and TS_value_cols[0]=='series_value'
+    
     print('TS_value_cols:', TS_value_cols)
     return TS_value_cols
 
 # verified to work 2/8/24
-# NOTE: we need this more complicated approach because data is messy so pd.infer_freq isinconsistent!
+# NOTE: we need this more complicated approach because data is messy so pd.infer_freq is inconsistent!
 def find_freq(df, datetime_col, unique_id_cols):
     if type(unique_id_cols) is str: unique_id_cols=[unique_id_cols]
     all_freqs=df.sort_values(unique_id_cols+[datetime_col]) \
@@ -33,41 +38,58 @@ def find_freq(df, datetime_col, unique_id_cols):
 
 # Verified to work 2/9/24
 # Master timeseries forecasting preprocessing function!
-def get_preprocessed_forecasting_data(df_raw, unique_id_col:str, max_df_len:int=None, 
-                                      valid_size=0.1, test_size=0.2, scale=False, fill_value=None):
+def get_preprocessed_forecasting_data(df_raw, unique_id_col:str, max_df_len:int=None, forecast_history:int=None,
+                                      forecast_horizon:int=None, valid_size=0.1, test_size=0.2, scale=False, fill_value=None):
+    """
+    gets preprocessed forecasting data in a simple way comparable to tsai's higher level classification api
+    :param df_raw: the raw df e.g. from monash data repository
+    :param max_df_len: max df len to avoid OOM
+    :param forecast_history: number of steps into the past for prediction (NOTE: auto selected if None)
+    :param forecast_horizon: number of steps into the future for prediction (NOTE: auto selected if None)
+    :param valid_size: proportion of validation data
+    :param test_size: proportion of test data
+    :param scale: whether to scale the data with the pipelines
+    :param fill_value: for filling missing values, defaults to ffill.
+    """
+    
     datetime_col = find_datetime_col(df_raw)
     TS_value_cols = find_TS_value_cols(df_raw) # this also might be constant? But regardless it's helpful to be prepared to multivariate datasets
     freq = find_freq(df_raw, datetime_col, unique_id_col)
 
     print('Example `pd.date_range()` constructed from given timeseries frequency & start datetime:')
-    print(pd.date_range(start=df_raw[datetime_col].iloc[0], freq=freq, periods=10))
+    print(pd.date_range(start=df_raw[datetime_col].iloc[0], freq=freq, periods=5))
     
     # NOTE: These preprocessing steps are apparently necessary; some of the Monash data is messy!
     method = 'ffill' if fill_value is None else None
     preproc_pipe = sklearn.pipeline.Pipeline([
         ('shrinker', TSShrinkDataFrame()), # shrink dataframe memory usage
-        ('drop_duplicates', TSDropDuplicates(datetime_col=datetime_col, unique_id_cols=unique_id_col)), # drop duplicate rows (if any), WARNING! Extreme df reduction here!
-        ('add_mts', TSAddMissingTimestamps(datetime_col=datetime_col, unique_id_cols=unique_id_col, freq=freq)), # ass missing timestamps (if any) (this is buggy!)
+        ('drop_duplicates', TSDropDuplicates(datetime_col=datetime_col, unique_id_cols=unique_id_col)), # drop duplicate rows (if any)
+        ('add_mts', TSAddMissingTimestamps(datetime_col=datetime_col, unique_id_cols=unique_id_col, freq=freq)), # ass missing timestamps (if any)
         ('fill_missing', TSFillMissing(columns=TS_value_cols, method=method, value=fill_value)), # fill missing data (1st ffill. 2nd value=0)
         ], verbose=True)
     df = preproc_pipe.fit_transform(df_raw)
     pipelines = [preproc_pipe]
     print('df.head():\n', df.head())
+    print('\ndf.dtypes:')
+    print(df.dtypes)
     
     # NOTE: reconstruct split distribution graph as seen for LTSF but for arbitrary dataset
     unique_subset_len = len(df)//len(df[unique_id_col].unique()) # need use len of unique subsets
-    fcst_history = unique_subset_len//8 # steps in the past (based on LTSF, possibly requires adjustment)
-    fcst_horizon = int(fcst_history*0.6) # steps in the future (based on LTSF, possibly requires adjustment)
+    if forecast_history is None: forecast_history = unique_subset_len//8 # steps in the past (based on LTSF, possibly requires adjustment)
+    if forecast_horizon is None: forecast_horizon = int(forecast_history*0.6) # steps in the future (based on LTSF, possibly requires adjustment)
 
     print('unique_subset_len:', unique_subset_len)
-    print('steps in the past:', fcst_history)
-    print('steps in the future:', fcst_horizon)
+    print('steps in the past:', forecast_history)
+    print('steps in the future:', forecast_horizon)
 
     # truncate dataset len sensibly using unique_subset_len
     df = df.sort_values([unique_id_col, datetime_col]).reset_index(drop=True)
     if max_df_len: df = df.iloc[:max_df_len-(max_df_len % unique_subset_len)]
+
+    print('\nlen(df_raw):', len(df_raw))
+    print('reduced df proportion:', len(df)/len(df_raw))
     
-    splits = get_forecasting_splits(df, fcst_history=fcst_history, fcst_horizon=fcst_horizon, datetime_col=datetime_col,
+    splits = get_forecasting_splits(df, fcst_history=forecast_history, fcst_horizon=forecast_horizon, datetime_col=datetime_col,
                                     unique_id_cols=unique_id_col, valid_size=valid_size, test_size=test_size, show_plot=True)
 
     if scale:
@@ -81,12 +103,27 @@ def get_preprocessed_forecasting_data(df_raw, unique_id_col:str, max_df_len:int=
     
     # NOTE: they mention this does a sliding window & I also saw there is a TSSlidingWindow (dataset) class
     # This makes me think that maybe TSDatasets & similar classes are lower-level & implicitly invoked by these higher level functions!
-    X, y = prepare_forecasting_data(df, fcst_history=fcst_history, fcst_horizon=fcst_horizon, x_vars=TS_value_cols, y_vars=TS_value_cols)
+    X, y = prepare_forecasting_data(df, fcst_history=forecast_history, fcst_horizon=forecast_horizon, 
+                                    x_vars=TS_value_cols, y_vars=TS_value_cols)
     return X, y, splits, pipelines
 
-def get_preprocessed_Monash_forecasting_data(dsid, *args, **kwd_args):
+def get_Monash_forecasting_data_cached(dsid):
+    print('All Monash Forecasting Datasets:\n', Monash_forecasting_list)
     print(f'Chosen Monash Forecasting Dataset: {dsid}')
-    df_raw = get_Monash_forecasting_data(dsid)
+    if dsid in get_Monash_forecasting_data_cached.cache:
+        print('Found the dataset in the cache!')
+        df_raw = get_Monash_forecasting_data_cached.cache[dsid]
+    else: 
+        print('Dataset not found in the cache...')
+        df_raw = get_Monash_forecasting_data(dsid)
+        get_Monash_forecasting_data_cached.cache[dsid]=df_raw
+    return df_raw
+get_Monash_forecasting_data_cached.cache={}
+
+def get_preprocessed_Monash_forecasting_data(dsid, *args, return_raw_df=False, **kwd_args):
+    df_raw = get_Monash_forecasting_data_cached(dsid) # load the data (possibly from cache)
     
     # unique_id_cols seems to be constant for Monash
-    return get_preprocessed_forecasting_data(df_raw, *args, unique_id_col = 'series_name', **kwd_args)
+    results = list(get_preprocessed_forecasting_data(df_raw, *args, unique_id_col = 'series_name', **kwd_args))
+    if return_raw_df: results.append(df_raw)
+    return results
--- fit_TSF_model.py
+++ fit_TSF_model.py
@@ -4,30 +4,42 @@ from tsai.basics import *
 import TS_forecast_preprocessing
 from jsonargparse import CLI # automatic CLI!!
 
-def _filter_kwargs(f):
-    import inspect
-    arg_names = inspect.getargspec(f).args
-    def _f(*args, **kwargs):
-        return f(*args, **{k: v for k, v in kwargs.items() if k in arg_names})
-    return _f
-
 # TODO: use with json argparse to include model hyper-params
 # This is cheatcodes to make jsonargparse work for everything!
 # NOTE: for InceptionTimePlus, you need to instead call this function on InceptionModulePlus
-def _fit_kwds_for(f, kwargs): # maybe better b/c it works even for dict config?
+def fit_kwds_for(f, kwargs): # maybe better b/c it works even for dict config?
     try: arg_names = inspect.getargspec(f).args
     except TypeError: return fit_kwds_for(f.__init__, kwargs)
     return {k: v for k, v in kwargs.items() if k in arg_names}
 
-def seed_everything(seed):
-    # seed everything
+def seed_everything(seed=None):
+    import random, torch, numpy as np
+    if seed is None: seed = random.randint(0,int(10000))
+    print('setting seed:', seed)
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+    return seed
 
-def fit_TSForecaster(dataset: str=None, n_epochs: int = 1, lr_max: float = 0.0025,
-        batch_size: int=16, max_df_len: int=100_000, scale=True, seed: int=None, fit_UQ=True, **kwd_args):
+# Verified to work: 2/12/24
+def recursive_TSForecaster(X, y, *args, arch, **kwd_args):
+    """ 
+    DONE! Just use the single_step_forecaster for training & the recursive one for evaluation!  (they share the same internal model)
+    You could even split the training between 1-step & recursive which would probably give good performance without tons of time. 
+    """
+    # first make fake forecaster with horizon=1 (uses complicated black-box model construction process)
+    single_step_forecaster = TSForecaster(X, y[:,:,:1], *args, arch=arch, **kwd_args)
+    single_step_model = single_step_forecaster.model # retrieve the model with horizon=1
+    assert len(y.shape)==3
+    fcst_horizon = y.shape[-1]
+    from tsai.models.misc import RecursiveWrapper
+    recursive_model = RecursiveWrapper(single_step_model, n_steps=fcst_horizon) # then make recursive model
+    return single_step_forecaster, TSForecaster(X, y, *args, arch=recursive_model, **kwd_args) # then pass into TSForecaster
+
+def fit_TSForecaster(dataset: str=None, n_epochs: int = 1, lr_max: float = 0.0025, batch_size: int=16, 
+        max_df_len: int=100_000, model_arch='InceptionTimePlus', arch_config={}, seed: int=None,
+        scale=True, fit_UQ=True, recursive=False, recursive_UQ=False, **kwd_args):
     """
     fits a TSForecasting model to a given dataset
     :param n_epochs: training epochs
@@ -35,56 +47,60 @@ def fit_TSForecaster(dataset: str=None, n_epochs: int = 1, lr_max: float = 0.002
     :param batch_size: train batch size
     :param max_df_len: some datasets are too big to fit into memory
     :param scale: whether to scale the dataset for easier training (it's invertible)
-    :param seed: rng seed for EVERYTHING (like PL)
     :param fit_UQ: whether to also a fit a UQ model to the primary regressor
+    :param recursive: whether to fit recursive models
+    :param seed: rng seed for EVERYTHING (like PL)
+    :param model_arch: type of model to fit
+    :param arch_config: dictionary of model configuration settings
     """
 
-    if seed is not None:
-        seed_everything(seed)
+    seed=seed_everything(seed)
 
+    prep_function = TS_forecast_preprocessing.get_preprocessed_Monash_forecasting_data
     if dataset is None:
         # NOTE: Monash is the ticket for forecasting! It has 30 different forecasting datasets!
         # This is a static list of possible Monash forecasting datasets!
-        print('All Monash Forecasting Datasets:\n', Monash_forecasting_list)
-        dataset=random.choice(Monash_forecasting_list) # TODO: make CLI arg & replace with df file path
-        print(f'...Randomly selecting Monash (dsid):', dataset)
-        prep_function = TS_forecast_preprocessing.get_preprocessed_Monash_forecasting_data
-    else:
-        dataset = pd.read_csv(dataset)
-        prep_function = TS_forecast_preprocessing.get_preprocessed_forecasting_data
+        dataset=random.choice(Monash_forecasting_list)
+        print(f'...Randomly selecting Monash (dsid):', dataset)        
+    
+    X, y, splits, pipelines, _ = prep_function(dataset, max_df_len=max_df_len, scale=scale, **kwd_args)
 
-    X, y, splits, pipelines = prep_function(dataset, max_df_len=max_df_len, scale=scale, **kwd_args)
+    ## NOTE: This is the full (default) arch_config directly from InceptionTimePlus.__init__() 
+    ## https://timeseriesai.github.io/tsai/models.inceptiontimeplus.html#inceptiontimeplus
+    #arch_config = dict(residual=True, depth=6, coord=False,
+    #                   norm='Batch', zero_norm=False, act=torch.nn.modules.activation.ReLU, act_kwargs={},
+    #                   sa=False, se=None, stoch_depth=1.0, ks=40,
+    #                   bottleneck=True, padding='same', separable=False,
+    #                   dilation=1, stride=1, conv_dropout=0.0, bn_1st=True)
+    #model_arch = "InceptionTimePlus" # default, make CLI arg?
 
-    # NOTE: This is the full (default) arch_config directly from InceptionTimePlus.__init__() 
-    # https://timeseriesai.github.io/tsai/models.inceptiontimeplus.html#inceptiontimeplus
-    arch_config = dict(residual=True, depth=6, coord=False,
-                       norm='Batch', zero_norm=False, act=torch.nn.modules.activation.ReLU, act_kwargs={},
-                       sa=False, se=None, stoch_depth=1.0, ks=40,
-                       bottleneck=True, padding='same', separable=False,
-                       dilation=1, stride=1, conv_dropout=0.0, bn_1st=True)
-    model_arch = "InceptionTimePlus" # default, make CLI arg?
+    TSForecaster_mean = recursive_TSForecaster if recursive else TSForecaster
+    TSForecaster_residual = recursive_TSForecaster if recursive_UQ else TSForecaster
+    if recursive_UQ: assert fit_UQ, "recursive_UQ can't be set if fit_UQ=False."
 
     # GOTCHA: sometimes the code will die here, the reason is that it runs OOM with a very large dataset!
     # in this case trying running again with another smaller dataset.
-    learn = TSForecaster(X, y, splits=splits, batch_size=batch_size, path="models", pipelines=pipelines,
-                         arch=model_arch, arch_config=arch_config, metrics=[mse, mae])#, cbs=ShowGraph())
+    learn = TSForecaster_mean(X, y, splits=splits, batch_size=batch_size, path="models", pipelines=pipelines,
+                         arch=model_arch, arch_config=arch_config, metrics=[mse, mae])
     # Why do they supply the pipelines again? are they reapplied? # it seems not based on lack of verbose output
     learn.dls.valid.drop_last = True
     print(learn.summary())
 
     print('fitting mean model:')
     learn.fit_one_cycle(n_epochs, lr_max=lr_max)
-    learn.export(f'{model_arch}_mean.pt')
+    postfix = '_recursive' if recursive else ''
+    learn.export(f'{model_arch}_mean{postfix}.pt')
     if fit_UQ:
         preds, *_ = learn.get_X_preds(X)
         residuals = y-to_np(preds) # this seems to be standard definition of residuals
-        residual_learn = TSForecaster(X, residuals, splits=splits, batch_size=batch_size, path="models", pipelines=pipelines,
-                                      arch=model_arch, arch_config=arch_config, metrics=[mse, mae])
+        residual_learn = TSForecaster_residual(X, residuals, splits=splits, batch_size=batch_size, path="models",
+                                    pipelines=pipelines, arch=model_arch, arch_config=arch_config, metrics=[mse, mae])
         residual_learn.dls.valid.drop_last = True
        
         print('fitting residual model:')
         residual_learn.fit_one_cycle(n_epochs, lr_max=lr_max)
-        residual_learn.export(f'{model_arch}_residual.pt')
+        postfix = '_recursive' if recursive_UQ else ''
+        residual_learn.export(f'{model_arch}_residual{postfix}.pt')
     
     return learn, residual_learn
 
